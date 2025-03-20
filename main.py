a7d4033fdcfa219dac1258e65031817fbdf7e407
Shameed Job <shameedjob@Shameeds-Mac-mini.local>
2025-03-20 08:16:10 -0400

    API Integrations

--- main.py
+++ main.py
@@ -14,90 +14,11 @@ except:
 
 app = Flask(__name__)
 
-def get_session_value(key):
-    if not key in session: return ""
-    else: return session[key]
-
-@app.route('/login')
-def login():
-    session["token"] = ""
-    session['email'] = ""
-    session['name'] = ""
-    return render_template('/login.html')
-
-def check_login(location):
-    if get_session_value("token") != "":
-        return location
-    else:
-        return redirect(url_for("login"))
-
-@app.route('/return')
-def login_return():
-    session['token'] =  request.args.get("id")
-    session['email'] =  request.args.get("email")
-    session['name'] =  request.args.get("name")
-    return redirect(url_for("home"))
-
-@app.route("/")
-def home():
-    return check_login(render_template("/home.html", name=get_session_value("name")))
-
-def logged_in():
-    return 'token' in session
-
-@app.route("/preferences")
-def preferences():
-    return check_login(render_template("/preferences.html"))
-
-def time_y(time):
-    return math.sin(time.year+time.month+time.day+datetime.datetime.now().second)
-    #return (time.year-2020)*12 + time.month-1
-
-@app.route('/create_list')
-def populate_data():
-    grocery_list = process_data.generate_grocery_list()
-    return render_template('/grocery_list.html', items=grocery_list)
-
-@app.route('/recipes')
-def recipes():
-    if not logged_in(): return redirect(url_for("home"))
-    
-    list = firebase_connection.get_node("groceryLists", session['email'])
-    if not list:  return redirect(url_for("home"))
-    recipes = []
-    for id in list["recipes"]:
-        response = recipe_api.get_recipe(id)
-        meal = response["meals"][0]
-        recipes.append({
-            "id": id,
-            "image": meal["strMealThumb"],
-            "name": meal["strMeal"],
-            "area":meal["strArea"],
-            "category":meal["strCategory"]
-        })
-
-
-    return check_login(render_template('/recipes.html', recipes=recipes))
-
-@app.route('/recipe')
-def recipe():
-    id = request.args.get('id')
-    response = recipe_api.get_recipe(id)
-    recipe = response["meals"][0]
-    recipe["items"] = []
-    for i in range(1,21):
-        if ("strIngredient"+str(i)) in recipe:
-            if recipe['strIngredient'+str(i)] == '': break
-            recipe["items"].append({"ingredient": recipe["strIngredient"+str(i)], "measure": recipe["strMeasure"+str(i)]})
-        else:  break
-    print(recipe)
-    return check_login(render_template('/recipe.html', recipe=recipe))
-
-@app.route('/charts')
-def populate():
-    return render_template('/charts.html')
-
+@app.route('/')
+def hello():
+    return render_template('/index.html')
 
 if __name__ == '__main__':
     app.secret_key = 'SECRET KEY'
     app.run(debug=True)
+
--- modules/api/bls_prices.py
+++ modules/api/bls_prices.py
@@ -54,7 +54,7 @@ api = BLSData()
 =======
 from get_prices_now import APIPriceConnection
 
-class BLSData(APIPriceConnection):
+class BLSData(get_prices_now.APIPriceConnection):
     #custom function to get BLS data
     def get_price_for(self, item):
         return super().get_price_for()
