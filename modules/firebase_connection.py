5988bed4a6acc24e57da3b3e1ada623adda3268f
Shameed Job <shameedjob@Shameeds-Mac-mini.local>
2025-04-04 19:16:01 -0400

    Including frontend templates

--- modules/firebase_connection.py
+++ modules/firebase_connection.py
@@ -1,5 +1,7 @@
-def get_price_winners():
-    pass
+import firebase_admin
 
 def get_all_tracked_items():
-    pass
+    data = ""
+    with open("./testdata.csv", "r") as file:
+        data = file.read()
+    return data
