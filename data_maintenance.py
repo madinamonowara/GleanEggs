00ecf7a4b71e5a59dcbc391859b63b93b7ecd8b8
Shameed Job <shameedjob@Shameeds-Mac-mini.local>
2025-04-21 00:19:40 -0400

    heroku maintenance

--- data_maintenance.py
+++ data_maintenance.py
@@ -1,6 +1,14 @@
 #USING THIS CLASS TO CREATE PROCEDURES IMPORTANT FOR MAINTENANCE OF APPLICATION
-
+import sys
 from modules.api import bls_prices
+import datetime
 
+def weekly_check():
+    if datetime.date.today().weekday == 6:
+        bls_prices.api.get_all_prices()
+    else:
+        print("Incorrect day, can not run.")
 
-bls_prices.api.get_all_prices()
+if __name__ == "__main__":
+    if sys.argv[1] == "weekly_check":
+        weekly_check()
